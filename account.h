b695298f10a869d46e8e35f78ac17a58006f5689
AlMavk <93182035+AlMavk@users.noreply.github.com>
2022-12-06 10:24:26 +0000

    Add yarn.lock file

--- account.h
+++ account.h
@@ -5,3 +5,6 @@
 
 EVP_PKEY* account_keys_create(void);
 int account_keys_write_PEM_file(EVP_PKEY* const key, uint8_t* const path);
+int account_keys_load_PEM_file(EVP_PKEY** const key, uint8_t* const filePath);
+void account_keys_print_one_PEM(EVP_PKEY* const key, KEY_TYPE KT);
+void account_keys_print_pair_PEM(EVP_PKEY* const key);
