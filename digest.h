97a1c45868e7636f2676e3afb37b53dc8c73eb8d
AlMavk <93182035+AlMavk@users.noreply.github.com>
2022-12-11 09:17:53 +0000

    Minor comment fix.

--- digest.h
+++ digest.h
@@ -1,3 +1,6 @@
 
 #pragma once
 #include "common.h"
+
+int digest_hash_file(uint8_t hashBinResult[SHA256_DIGEST_LENGTH], uint8_t* const filePath);
+int digest_hash_str(uint8_t binmd[SHA256_DIGEST_LENGTH], uint8_t* const str);                                                            // 256 bits, 32 bytes
