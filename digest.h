0d2f2360d61e316fed40ea5ec7fe456c3875455f
AlMavk <93182035+AlMavk@users.noreply.github.com>
2022-12-17 05:31:32 +0000

    Added LICENSE file

--- digest.h
+++ digest.h
@@ -4,3 +4,5 @@
 
 int digest_hash_file(uint8_t hashBinResult[SHA256_DIGEST_LENGTH], uint8_t* const filePath);
 int digest_hash_str(uint8_t binmd[SHA256_DIGEST_LENGTH], uint8_t* const str);                                                            // 256 bits, 32 bytes
+int digest_hash_bin(uint8_t ouput_bin_buffer[SHA256_DIGEST_LENGTH], uint8_t const input_bin_buf[SHA256_DIGEST_LENGTH]);                  // 256 bits, 32 bytes
+int digest_bin_2_hex(uint8_t hexmd[HEX_HASH_NT_SZ], IO_BUFFER_SZ OUT_SZ, uint8_t const binmd[SHA256_DIGEST_LENGTH], IO_BUFFER_SZ IN_SZ); // TODO : simplifier : lock buffer size, suppress arg 2 & 4
