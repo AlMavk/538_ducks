92678ec0327972a7f25eaf068bf491056dc77e7e
AlMavk <93182035+AlMavk@users.noreply.github.com>
2022-12-17 19:14:25 +0000

    Added entry.

--- digest.h
+++ digest.h
@@ -8,3 +8,4 @@ int digest_hash_bin(uint8_t ouput_bin_buffer[SHA256_DIGEST_LENGTH], uint8_t cons
 int digest_bin_2_hex(uint8_t hexmd[HEX_HASH_NT_SZ], IO_BUFFER_SZ OUT_SZ, uint8_t const binmd[SHA256_DIGEST_LENGTH], IO_BUFFER_SZ IN_SZ); // TODO : simplifier : lock buffer size, suppress arg 2 & 4
 uint8_t* digest_hex_2_bin_bulk(uint8_t* bin_array, uint8_t* const const hexmd[HEX_HASH_NT_SZ], size_t count);
 uint8_t* digest_hex_2_bin_bulk_to_lendian(uint8_t* const hexmd[], size_t count);
+// int digest_hex_2_bin(uint8_t binmd[SHA256_DIGEST_LENGTH], uint8_t const hexmd[HEX_HASH_NT_SZ], size_t count);		// a refaire pour un seul md
