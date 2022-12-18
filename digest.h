4d8deda080c3faa6d0d3daca332d45eb70427164
AlMavk <93182035+AlMavk@users.noreply.github.com>
2022-12-18 06:30:54 +0000

    docs fixed

--- digest.h
+++ digest.h
@@ -9,3 +9,6 @@ int digest_bin_2_hex(uint8_t hexmd[HEX_HASH_NT_SZ], IO_BUFFER_SZ OUT_SZ, uint8_t
 uint8_t* digest_hex_2_bin_bulk(uint8_t* bin_array, uint8_t* const const hexmd[HEX_HASH_NT_SZ], size_t count);
 uint8_t* digest_hex_2_bin_bulk_to_lendian(uint8_t* const hexmd[], size_t count);
 // int digest_hex_2_bin(uint8_t binmd[SHA256_DIGEST_LENGTH], uint8_t const hexmd[HEX_HASH_NT_SZ], size_t count);		// a refaire pour un seul md
+void digest_hex_pretty_print(uint8_t const hexsig[]);
+
+// merkle tree functions
