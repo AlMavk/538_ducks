229a02fac8f94044b14ec684d5ef81ca9973903b
AlMavk <93182035+AlMavk@users.noreply.github.com>
2023-02-11 06:57:49 +0000

    fix formatting issues

--- sign.h
+++ sign.h
@@ -5,3 +5,4 @@
 
 int sign_msg(uint8_t signBin[SIG_BIN_SZ], EVP_PKEY* const skey, uint8_t* const msg);
 int sign_file(uint8_t signBin[SIG_BIN_SZ], EVP_PKEY* const skey, uint8_t* const filePath);
+bool sign_verify_str_msg(EVP_PKEY* const pubkey, uint8_t* const signature, uint8_t* const msg);
