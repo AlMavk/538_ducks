cc9c7ff7f537b73d4f7790b61c8371b0b8439cde
AlMavk <93182035+AlMavk@users.noreply.github.com>
2023-02-11 05:35:51 +0000

    compiles again

--- sign.h
+++ sign.h
@@ -2,3 +2,6 @@
 #pragma once
 
 #include "common.h"
+
+int sign_msg(uint8_t signBin[SIG_BIN_SZ], EVP_PKEY* const skey, uint8_t* const msg);
+int sign_file(uint8_t signBin[SIG_BIN_SZ], EVP_PKEY* const skey, uint8_t* const filePath);
