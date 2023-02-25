427274cadb896cf0755bfe70c208283285bc3d84
AlMavk <93182035+AlMavk@users.noreply.github.com>
2023-02-25 11:27:39 +0000

    Fix merge conflict

--- utils.c
+++ utils.c
@@ -9,3 +9,4 @@ uint8_t* concat(uint8_t* const strA, uint8_t* const strB)
 		return NULL;
 
 	result = malloc(strlen(strA) + strlen(strB) + 1);	// +1 for the null terminator;
+	if (result == NULL) exit(-1);
